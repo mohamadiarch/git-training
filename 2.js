7ed0537957837a4cfc2da39eacedb8ed2840c77d
mohamad <mohammadmohammadi7613@gmail.com>
2023-05-07 22:09:47 +0430

    branch b1

--- 2.js
+++ 2.js
@@ -1,3 +1,5 @@
 console.log(" this si s file two")
 console.log("THis is one change with no enter")
 console.log("object")
+
+console.log("I change this in branch b2")
--- 3.js
+++ 3.js
@@ -1,1 +1,3 @@
 console.log("this is file tree")
+
+console.log("I change in branch b2`")
