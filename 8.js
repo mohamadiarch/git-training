ce0d74f43addc991e975a239be9dd94a597f65f7
mohamad <mohammadmohammadi7613@gmail.com>
2023-05-08 12:42:37 +0430

    commit in master

--- 8.js
+++ 8.js
@@ -4,3 +4,7 @@ console.log("I am create this in branch master")
 console.log("change in branch b2")
 
 console.log("I am changing  this file in branch 2")
+
+
+
+console.log("I want to copy master in b3")
